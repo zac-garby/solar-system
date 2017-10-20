99ca1ea678fc76f8514f726b0dee13338d9216c7
David Hausner <DavidRHausner@users.noreply.github.com>
2017-10-20 09:35:17 -0500

    Update planet.h
    Added function to calculate acceleration due to gravity at planet's surface relative to Earth's acceleration due to gravity at planet's surface, and a function to calculate orbital period based on distanceFromSun.
    
    It may be helpful to stick to SI units (m, kg, s) instead of using yottagrams for planets' masses, miles for distanceFromSun, and m, g, and s for GRAVITY_CONST. This way, forces could be calculated in Newtons across the board without the need for conversions.

--- planet.h
+++ planet.h
@@ -51,12 +51,19 @@ public:
     float earthLikeness;    // How likely the planet is to Earth (1f == Earth)
     float radius;           // x10^2AU
     float distanceFromSun;  // miles
+    float orbitalPeriod;    // in time increments, should be calculated by Kepler's Third Law using distanceFromSun
     float mass;             // yg (yottagrams)
     float angle;            // º
-    float speed;            // px/s
+    float speed;            // px/s, calculated from Kepler's Second Law using distanceFromSun
     float biodiversity;	    // between 1-10
     float capacity;         // capacity
     float gravity;          // relative to Earth's g
+    float airPressureSurface; // relative to Earth's atm, at surface
+    float atmosphereHeight; // in miles, pressure drops linearly to 0 at altitude==atmosphereHeight
+    float albedo;           // between 0 and 1
+    float surfaceTempEquator; // function of albedo, distanceFromSun
+    int moons;              // larger planets tend to have more
+    int rings;              // 0 for none, else display n rings as concentric circles above equator
 
     // Explicit since there's only one parameter.
     explicit Planet(float radius);
@@ -71,6 +78,35 @@ public:
 
     // Calculates the planet border's radius in pixels
     float getBorderPixelRadius();
+    
+    // Calculates the planet's orbital period in game minutes using Kepler's Third Law
+    float getOrbitalPeriod()
+    {
+        // convert distanceFromSun in miles to AU
+        float distanceInAU = distanceFromSun * 1.60934f; // miles to kilometers
+        distanceInAU *= 1000; // kilometers to meters
+        distanceInAU /= AU_TO_METER; // meters to AU
+        // Kepler's Third Law
+        float periodInYears = (float)sqrt((double)(distanceInAU * distanceInAU * distanceInAU));
+        return (periodInYears * TIMESCALE);
+    }
+    
+    // Calculates acceleration due to gravity at surface relative to Earth's acceleration due to gravity
+    float getGravity()
+    {
+        /* 
+           m1 = mass of object on planet's surface
+           m2 = mass of planet
+           a = acceleration due to planet's gravity
+           F = m1a and F = G(m1m2)/(r^2)
+           m1a = G(m1m2)/(r^2)
+           a = G*m2/(r^2)
+        */
+        float a = GRAVITY_CONST; // uses meters, grams, seconds
+        a *= mass * 1.0E24f; // convert yottagrams to grams
+        a /= (radius * radius); // acceleration due to gravity in meters per second squared
+        return a / EARTH_G; // returns acceleration due to gravity as a multiple of Earth's acceleration due to gravity
+    }
 
     // Checks whether the given vector is inside
     // the planet's bounds
